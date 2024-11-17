71b431d6c57c0edb99ebed59e95584893244bb22
Yeleti Nandhu reddy <nandu.intern@gmail.com>
2024-11-17 09:12:46 +0530

    Day 3

---
+++ ReverseArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void reverseArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverseArray(arr);
+
+        System.out.print("Reversed Array is: ");
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        sc.close();
+    }
+}
