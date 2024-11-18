91b6140c95c1bc15b5817df071213e7786ad6b0c
Yeleti Nandhu reddy <nandu.intern@gmail.com>
2024-11-18 08:14:37 +0530

    Day 4

---
+++ RotateArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RotateArray {
+    static void rotateArr(int arr[], int d) {
+        int n = arr.length;
+        d = d % n;
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    static void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.print("Enter the number of rotations: ");
+        int d = scanner.nextInt();
+        rotateArr(arr, d);
+        System.out.println("Rotated Array: " + Arrays.toString(arr));
+    }
+}
